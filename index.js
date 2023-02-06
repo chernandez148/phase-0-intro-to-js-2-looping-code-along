e0ba23d9e5b6e40486d1537be38a7f24bf38767d
Christian Hernandez <hernandezchristian94@gmail.com>
2023-02-06 09:18:42 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+
+function writeCards(namesArray, event) {
+  let thankYouCards = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    thankYouCards.push(
+      `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return thankYouCards;
+}
+console.log(writeCards());
+
+function countDown() {
+  let countDown = 10;
+  while (countDown > -1) {
+    console.log(countDown--);
+  }
+}
+console.log(countDown());
